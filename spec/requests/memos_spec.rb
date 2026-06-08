b9ac6e1d34fe4b1daa7b57c5572f4bf8bc26c632
aprict776 <aaaaaaaaaa111111111james@gmail.com>
2026-06-08 11:31:39 +0900

    コンフリクト解消4

--- spec/requests/memos_spec.rb
+++ spec/requests/memos_spec.rb
@@ -31,7 +31,8 @@ RSpec.describe "Memos", type: :request do
     it "メモを作成できる" do
       expect {
         post memos_path, params: { memo: { content: "テストメモ" } }
-       }.to change(Memo, :count).by(1)  # メモの数が1増えることを確認
+
+      }.to change(Memo, :count).by(1)  # メモの数が1増えることを確認
     end
 
     it "空のメモは作成できない" do
@@ -55,42 +56,6 @@ RSpec.describe "Memos", type: :request do
       expect {
         delete memo_path(other_memo)
       }.not_to change(Memo, :count)
-    end
-  end
-
-  describe "GET /memos/:id/edit" do
-    it "自分のメモの編集画面を表示できる" do
-      memo = create(:memo, user: user)
-      get edit_memo_path(memo)
-      expect(response).to have_http_status(:ok)
-    end
-
-    it "他のユーザーのメモの編集画面は表示できない" do
-      other_user = create(:user)
-      other_memo = create(:memo, user: other_user)
-      get edit_memo_path(other_memo)
-      expect(response).not_to have_http_status(:ok)
-    end
-  end
-
-  describe "PATCH /memos/:id" do
-    it "自分のメモを更新できる" do
-      memo = create(:memo, user: user)
-      patch memo_path(memo), params: { memo: { content: "更新後のメモ" } }
-      expect(memo.reload.content).to eq "更新後のメモ"
-    end
-
-    it "空のメモには更新できない" do
-      memo = create(:memo, user: user)
-      patch memo_path(memo), params: { memo: { content: "" } }
-      expect(memo.reload.content).not_to eq ""
-    end
-
-    it "他のユーザーのメモは更新できない" do
-      other_user = create(:user)
-      other_memo = create(:memo, user: other_user)
-      patch memo_path(other_memo), params: { memo: { content: "更新後のメモ" } }
-      expect(other_memo.reload.content).not_to eq "更新後のメモ"
     end
   end
 end
