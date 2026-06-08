e50b93406cb8b56e4dd1440bd849f16219e1e7fd
aprict776 <aaaaaaaaaa111111111james@gmail.com>
2026-06-08 10:00:44 +0900

    メモ編集機能追加

--- spec/requests/memos_spec.rb
+++ spec/requests/memos_spec.rb
@@ -56,5 +56,41 @@ RSpec.describe "Memos", type: :request do
         delete memo_path(other_memo)
       }.not_to change(Memo, :count)
     end
+
+    describe "GET /memos/:id/edit" do
+  it "自分のメモの編集画面を表示できる" do
+    memo = create(:memo, user: user)
+    get edit_memo_path(memo)
+    expect(response).to have_http_status(:ok)
   end
+
+  it "他のユーザーのメモの編集画面は表示できない" do
+    other_user = create(:user)
+    other_memo = create(:memo, user: other_user)
+    get edit_memo_path(other_memo)
+    expect(response).not_to have_http_status(:ok)
+  end
+end
+
+describe "PATCH /memos/:id" do
+  it "自分のメモを更新できる" do
+    memo = create(:memo, user: user)
+    patch memo_path(memo), params: { memo: { content: "更新後のメモ" } }
+    expect(memo.reload.content).to eq "更新後のメモ"
+  end
+
+  it "空のメモには更新できない" do
+    memo = create(:memo, user: user)
+    patch memo_path(memo), params: { memo: { content: "" } }
+    expect(memo.reload.content).not_to eq ""
+  end
+
+  it "他のユーザーのメモは更新できない" do
+    other_user = create(:user)
+    other_memo = create(:memo, user: other_user)
+    patch memo_path(other_memo), params: { memo: { content: "更新後のメモ" } }
+    expect(other_memo.reload.content).not_to eq "更新後のメモ"
+  end
+end
+end
 end
