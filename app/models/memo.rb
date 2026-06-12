4c92724ba85b277ec72815c18dabaefbb49e9370
aprict776 <aaaaaaaaaa111111111james@gmail.com>
2026-06-12 19:50:29 +0900

    ログイン画面のin18/メモ削除機能のエラー解消

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -1,5 +1,9 @@
 class Memo < ApplicationRecord
   belongs_to :user
 
+  # メモ削除時にmemo_creationsも一緒に削除する
+  has_many :memo_creations, dependent: :destroy
+  has_many :creations, through: :memo_creations
+
   validates :content, presence: true
 end
